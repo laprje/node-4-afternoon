375a28035cd0e222e878f861b881929d586a52aa
L J <laprje@gmail.com>
2019-11-04 16:17:20 -0700

    changed format and some syntax to match with detailed instructions

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -10,21 +10,33 @@ module.exports = {
         const user = users.find(
             user => user.username === username && user.password === password
         );
+
+        if(user) {
+            session.user.username = user.username;
+            res.status(200).send(session.user);
+        } else {
+            res.status(500).send("Unauthorized.")
+        }
     },
 
     register: (req, res) => {
-        if(req.body.username && req.body.password) {
-            let user = req.session.user;
-            user.id = id;
-            users.push(user)
-            id++;
-            session.user.username = username;
-            res.status(200).send(req.session.user)
-        }
+        const { session } = req;
+        const { username, password } = req.body
+
+        users.push({ id, username, password })
+        id++;
+
+        session.user.username = username
+
+        res.status(200).send(session.user)
     },
 
     signout: (req, res) => {
         req.session.destroy();
-        return(req.session);
+        res.status(200).send(req.session);
+    },
+
+    getUser: (req, res) => {
+        res.status(200).send(req.session.user)
     }
 }
